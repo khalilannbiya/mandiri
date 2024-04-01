476545166a5be95a61c32cd1e5750ba8cd36bdb4
Syeich Khalil Annbiya <84386261+khalilannbiya@users.noreply.github.com>
2024-04-01 10:26:24 +0700

    feat: Adding active style for the currently active navlink (#20)
    Co-authored-by: syeichkhalil30@gmail.com <syeichkhalil@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./vendor/laravel/jetstream/**/*.blade.php",
-        "./storage/framework/views/*.php",
-        "./resources/views/**/*.blade.php",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
     ],
 
     theme: {
         extend: {
             colors: {
-                primary: "#94A3B8",
-                white: "#FFFFFF",
-                black: "#000000",
+                primary: '#94A3B8',
+                white: '#FFFFFF',
+                black: '#000000',
             },
             fontFamily: {
-                rubik: ["Rubik", ...defaultTheme.fontFamily.sans],
-                epilogue: ["Epilogue", ...defaultTheme.fontFamily.sans],
+                rubik: ['Rubik', ...defaultTheme.fontFamily.sans],
+                epilogue: ['Epilogue', ...defaultTheme.fontFamily.sans],
             },
         },
     },
 
     plugins: [
-        require("@tailwindcss/forms"),
-        require("@tailwindcss/typography"),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
     ],
-};
+}
