c7ba60dbbc8ebc2b0b44a756e68804849e46d2cd
Syeich Khalil Annbiya <84386261+khalilannbiya@users.noreply.github.com>
2024-04-01 11:45:57 +0700

    fix: Image slider size issues, move description, responsive breadcrumb & header (#21)
    Co-authored-by: syeichkhalil30@gmail.com <syeichkhalil@gmail.com>

--- resources/views/pages/frontend/product-detail.blade.php
+++ resources/views/pages/frontend/product-detail.blade.php
@@ -1,154 +1,139 @@
 @extends('layouts.frontend')
 
 @section('title')
-Detail Produk —
+    Detail Produk —
 @endsection
 
 @section('content')
-<section class="pt-[54px]">
-    <div
-        class="bg-primary aspect-square lg:aspect-auto h-[300px] w-full flex flex-col items-center justify-center gap-4">
-        <h1 class="text-4xl font-bold text-white">{{ $product->name }}</h1>
-        <nav class="flex" aria-label="Breadcrumb">
-            <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-                <li class="inline-flex items-center">
-                    <a href="{{ route('index') }}"
-                        class="inline-flex items-center text-sm font-medium text-white hover:text-slate-300">
-                        <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor" viewBox="0 0 20 20">
-                            <path
-                                d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
-                        </svg>
-                        Beranda
-                    </a>
-                </li>
-                <li>
-                    <div class="flex items-center">
-                        <svg class="rtl:rotate-180 w-3 h-3 text-white mx-1" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m1 9 4-4-4-4" />
-                        </svg>
-                        <a href="{{ route('show-by-category', $product->category->slug) }}"
-                            class="ms-1 text-sm font-medium text-white hover:text-slate-300 md:ms-2">{{
-                            $product->category->name }}</a>
-                    </div>
-                </li>
-                <li aria-current="page">
-                    <div class="flex items-center">
-                        <svg class="rtl:rotate-180 w-3 h-3 text-white mx-1" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m1 9 4-4-4-4" />
-                        </svg>
-                        <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2">{{ $product->name }}</span>
-                    </div>
-                </li>
-            </ol>
-        </nav>
-    </div>
-</section>
-<section class="flex flex-col lg:flex-row justify-between py-10">
-    <section class="w-full lg:w-[25%] xl-[20%] flex justify-center">
-
-        {{-- Categories List --}}
-        @include('components.frontend.categories-list')
-
+    <section class="pt-[54px]">
+        <div
+            class="bg-primary aspect-square lg:aspect-auto h-[300px] w-full flex flex-col items-center justify-center gap-4">
+            <h1 class="text-4xl font-bold text-center leading-normal text-white">{{ $product->name }}</h1>
+            <nav class="flex" aria-label="Breadcrumb">
+                <ol class="inline-flex items-center justify-center flex-wrap space-x-1 md:space-x-2 rtl:space-x-reverse">
+                    <li class="inline-flex items-center">
+                        <a href="{{ route('index') }}"
+                            class="inline-flex items-center text-sm font-medium text-white hover:text-slate-300">
+                            <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor" viewBox="0 0 20 20">
+                                <path
+                                    d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                            </svg>
+                            Beranda
+                        </a>
+                    </li>
+                    <li>
+                        <div class="flex items-center">
+                            <svg class="rtl:rotate-180 w-3 h-3 text-white mx-1" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="m1 9 4-4-4-4" />
+                            </svg>
+                            <a href="{{ route('show-by-category', $product->category->slug) }}"
+                                class="ms-1 text-sm font-medium text-white hover:text-slate-300 md:ms-2">{{ $product->category->name }}</a>
+                        </div>
+                    </li>
+                    <li aria-current="page">
+                        <div class="flex items-center">
+                            <svg class="rtl:rotate-180 w-3 h-3 text-white mx-1" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="m1 9 4-4-4-4" />
+                            </svg>
+                            <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2">{{ $product->name }}</span>
+                        </div>
+                    </li>
+                </ol>
+            </nav>
+        </div>
     </section>
-    <section
-        class="w-full lg:w-[75%] py-16 md:py-6 lg:py-10 flex flex-wrap justify-center lg:justify-start items-center">
-        <div class="bg-primary w-full flex justify-center items-center rounded-lg">
-            <div id="gallery" class="relative w-[65%] py-6 md:py-3 lg:py-0" data-carousel="slide">
-                <!-- Carousel wrapper -->
-                <div class="relative h-[16rem] sm:h-[20rem] overflow-hidden rounded-lg md:h-96">
-                    <!-- Item 1 -->
-                    @forelse ($product->productGalleries as $image)
-                    <div class="hidden duration-700 ease-in-out rounded-lg"
-                        data-carousel-item="{{ $image[0] ? 'active':'' }}">
-                        <img src="{{ Storage::url($image->url) }}"
-                            class="absolute block max-w-full object-cover object-center aspect-square md:aspect-video h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-lg"
-                            alt="Foto Produk {{ $product->name }}">
+    <section class="flex flex-col lg:flex-row justify-between py-10">
+        <section class="w-full lg:basis-1/4 flex justify-center">
+
+            {{-- Categories List --}}
+            @include('components.frontend.categories-list')
+
+        </section>
+        <section class="w-full lg:basis-3/4 py-16 md:py-6 lg:py-10 flex flex-wrap justify-start items-center">
+            <div class="bg-primary w-full flex justify-center items-center rounded-lg">
+                <div id="gallery" class="relative w-[65%] py-6 md:py-3 lg:py-0" data-carousel="slide">
+                    <!-- Carousel wrapper -->
+                    <div class="relative h-[16rem] sm:h-[20rem] overflow-hidden rounded-lg md:h-96">
+                        @forelse ($product->productGalleries as $image)
+                            <div class="hidden duration-700 ease-in-out rounded-lg"
+                                data-carousel-item="{{ $image[0] ? 'active' : '' }}">
+                                <img src="{{ Storage::url($image->url) }}"
+                                    class="absolute block max-w-full object-cover object-center aspect-square md:aspect-video h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-lg"
+                                    alt="Foto Produk {{ $product->name }}">
+                            </div>
+                        @empty
+                            <div class="hidden duration-700 ease-in-out rounded-lg" data-carousel-item>
+                                <img src="{{ asset('assets/images/default-image.png') }}"
+                                    class="absolute block max-w-full object-cover object-center aspect-square md:aspect-video h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-lg"
+                                    alt="Tidak Ada Foto Produk">
+                            </div>
+                        @endforelse
                     </div>
-                    @empty
-                    <div class="hidden duration-700 ease-in-out rounded-lg" data-carousel-item>
-                        <img src="{{ asset('assets/images/default-image.png') }}"
-                            class="absolute block max-w-full object-cover object-center aspect-square md:aspect-video h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-lg"
-                            alt="Tidak Ada Foto Produk">
-                    </div>
-                    @endforelse
-                    <!-- Item 2 -->
-                    <div class="hidden duration-700 ease-in-out rounded-lg" data-carousel-item>
-                        <img src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
-                            class="absolute block max-w-full object-cover object-center aspect-square md:aspect-video h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-lg"
-                            alt="">
-                    </div>
-                    <!-- Item 3 -->
-                    <div class="hidden duration-700 ease-in-out rounded-lg" data-carousel-item>
-                        <img src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
-                            class="absolute block max-w-full object-cover object-center aspect-square md:aspect-video h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-lg"
-                            alt="">
-                    </div>
+                    <!-- Slider controls -->
+                    <button type="button"
+                        class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                        data-carousel-prev>
+                        <span
+                            class="inline-flex items-center justify-center w-8 h-8 md:w-10 md:h-10 rounded-full bg-gray-800/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none transition-all duration-300 ease-in-out">
+                            <svg class="w-3 h-3 md:w-4 md:h-4 text-white group-hover:text-slate-300 rtl:rotate-180"
+                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M5 1 1 5l4 4" />
+                            </svg>
+                            <span class="sr-only">Previous</span>
+                        </span>
+                    </button>
+                    <button type="button"
+                        class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                        data-carousel-next>
+                        <span
+                            class="inline-flex items-center justify-center w-8 h-8 md:w-10 md:h-10 rounded-full bg-gray-800/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none transition-all duration-300 ease-in-out">
+                            <svg class="w-3 h-3 md:w-4 md:h-4 text-white group-hover:text-slate-300 rtl:rotate-180"
+                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="m1 9 4-4-4-4" />
+                            </svg>
+                            <span class="sr-only">Next</span>
+                        </span>
+                    </button>
                 </div>
-                <!-- Slider controls -->
-                <button type="button"
-                    class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-                    data-carousel-prev>
-                    <span
-                        class="inline-flex items-center justify-center w-8 h-8 md:w-10 md:h-10 rounded-full bg-gray-800/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none transition-all duration-300 ease-in-out">
-                        <svg class="w-3 h-3 md:w-4 md:h-4 text-white group-hover:text-slate-300 rtl:rotate-180"
-                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M5 1 1 5l4 4" />
-                        </svg>
-                        <span class="sr-only">Previous</span>
-                    </span>
-                </button>
-                <button type="button"
-                    class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-                    data-carousel-next>
-                    <span
-                        class="inline-flex items-center justify-center w-8 h-8 md:w-10 md:h-10 rounded-full bg-gray-800/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none transition-all duration-300 ease-in-out">
-                        <svg class="w-3 h-3 md:w-4 md:h-4 text-white group-hover:text-slate-300 rtl:rotate-180"
-                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m1 9 4-4-4-4" />
-                        </svg>
-                        <span class="sr-only">Next</span>
-                    </span>
-                </button>
             </div>
-        </div>
-        <div class="flex flex-col item-center py-6 gap-8 sm:pr-6 md:pr-8 lg:pr-12">
-            <h1
-                class="relative text-2xl font-bold after:absolute after:bottom-[-25px] after:left-0 after:w-[80px] after:h-[4px] after:rounded-md after:bg-primary">
-                {{ $product->name }}</h1>
-            <section class="mt-4">
-                {!! $product->description !!}
-            </section>
-            <div class="flex items-center">
-                <a href=""
-                    class="flex items-center justify-between px-4 gap-3 select-none rounded-lg bg-primary py-3 text-center align-middle text-sm font-bold text-white shadow-md shadow-slate-500/20 transition-all hover:shadow-lg hover:shadow-slate-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none transition-all duration-500 ease-in-out">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                        style="fill: white;transform: ;msFilter:;">
-                        <path
-                            d="M21.822 7.431A1 1 0 0 0 21 7H7.333L6.179 4.23A1.994 1.994 0 0 0 4.333 3H2v2h2.333l4.744 11.385A1 1 0 0 0 10 17h8c.417 0 .79-.259.937-.648l3-8a1 1 0 0 0-.115-.921zM17.307 15h-6.64l-2.5-6h11.39l-2.25 6z">
-                        </path>
-                        <circle cx="10.5" cy="19.5" r="1.5"></circle>
-                        <circle cx="17.5" cy="19.5" r="1.5"></circle>
-                    </svg>
-                    Beli Sekarang
-                </a>
+            <div class="flex flex-col item-center py-6 gap-8 sm:pr-6 md:pr-8 lg:pr-12">
+                <h1
+                    class="relative text-2xl font-bold after:absolute after:bottom-[-25px] after:left-0 after:w-[80px] after:h-[4px] after:rounded-md after:bg-primary">
+                    {{ $product->name }}</h1>
+                <section class="mt-4">
+                    {!! $product->description !!}
+                </section>
+                <div class="flex items-center">
+                    <a href=""
+                        class="flex items-center justify-between px-4 gap-3 select-none rounded-lg bg-primary py-3 text-center align-middle text-sm font-bold text-white shadow-md shadow-slate-500/20 transition-all hover:shadow-lg hover:shadow-slate-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none duration-500 ease-in-out">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            style="fill: white;transform: ;msFilter:;">
+                            <path
+                                d="M21.822 7.431A1 1 0 0 0 21 7H7.333L6.179 4.23A1.994 1.994 0 0 0 4.333 3H2v2h2.333l4.744 11.385A1 1 0 0 0 10 17h8c.417 0 .79-.259.937-.648l3-8a1 1 0 0 0-.115-.921zM17.307 15h-6.64l-2.5-6h11.39l-2.25 6z">
+                            </path>
+                            <circle cx="10.5" cy="19.5" r="1.5"></circle>
+                            <circle cx="17.5" cy="19.5" r="1.5"></circle>
+                        </svg>
+                        Beli Sekarang
+                    </a>
+                </div>
             </div>
-        </div>
+        </section>
     </section>
-</section>
 @endsection
 
 @push('script')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
 
-<script>
-    document.addEventListener("DOMContentLoaded", function(evt) {
+    <script>
+        document.addEventListener("DOMContentLoaded", function(evt) {
 
             document.querySelectorAll(".title").forEach(function(el) {
                 el.addEventListener("click", function(evt) {
@@ -174,5 +159,5 @@ Detail Produk —
             });
 
         });
-</script>
+    </script>
 @endpush
--- resources/views/pages/frontend/products-by-category.blade.php
+++ resources/views/pages/frontend/products-by-category.blade.php
@@ -1,71 +1,70 @@
 @extends('layouts.frontend')
 
 @section('title')
-Produk Kami —
+    Produk Kami —
 @endsection
 
 @section('content')
-<section class="pt-[54px]">
-    <div
-        class="bg-slate-300 aspect-square lg:aspect-auto h-[300px] w-full flex flex-col items-center justify-center gap-4">
-        <h1 class="text-4xl font-bold">{{ $category->name }}</h1>
-        <nav class="flex" aria-label="Breadcrumb">
-            <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-                <li class="inline-flex items-center">
-                    <a href="{{ route('index') }}"
-                        class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-white">
-                        <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor" viewBox="0 0 20 20">
-                            <path
-                                d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
-                        </svg>
-                        Beranda
-                    </a>
-                </li>
-                <li aria-current="page">
-                    <div class="flex items-center">
-                        <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m1 9 4-4-4-4" />
-                        </svg>
-                        <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2">{{ $category->name }}</span>
-                    </div>
-                </li>
-            </ol>
-        </nav>
-    </div>
-</section>
-<section class="flex flex-col lg:flex-row justify-between py-10">
-    <section class="w-full lg:w-[25%] xl-[20%] flex justify-center">
-
-        {{-- Categories List --}}
-        @include('components.frontend.categories-list')
-
+    <section class="pt-[54px]">
+        <div
+            class="bg-slate-300 aspect-square lg:aspect-auto h-[300px] w-full flex flex-col items-center justify-center gap-4">
+            <h1 class="text-4xl text-center leading-normal font-bold">{{ $category->name }}</h1>
+            <nav class="flex" aria-label="Breadcrumb">
+                <ol class="inline-flex items-center justify-center flex-wrap space-x-1 md:space-x-2 rtl:space-x-reverse">
+                    <li class="inline-flex items-center">
+                        <a href="{{ route('index') }}"
+                            class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-white">
+                            <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor" viewBox="0 0 20 20">
+                                <path
+                                    d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                            </svg>
+                            Beranda
+                        </a>
+                    </li>
+                    <li aria-current="page">
+                        <div class="flex items-center">
+                            <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="m1 9 4-4-4-4" />
+                            </svg>
+                            <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2">{{ $category->name }}</span>
+                        </div>
+                    </li>
+                </ol>
+            </nav>
+        </div>
     </section>
-    <section
-        class="w-full lg:w-[75%] py-16 md:py-6 flex flex-wrap justify-center lg:justify-start items-center gap-y-20 gap-x-8 xl:gap-x-12">
-        @forelse ($products as $product)
+    <section class="flex flex-col lg:flex-row justify-between py-10">
+        <section class="w-full lg:w-[25%] xl-[20%] flex justify-center">
 
-        {{-- Card Product --}}
-        @include('components.frontend.card-product')
+            {{-- Categories List --}}
+            @include('components.frontend.categories-list')
 
-        @empty
-        <h2>belum ada product</h2>
-        @endforelse
+        </section>
+        <section
+            class="w-full lg:w-[75%] py-16 md:py-6 flex flex-wrap justify-center lg:justify-start items-center gap-y-20 gap-x-8 xl:gap-x-12">
+            @forelse ($products as $product)
+                {{-- Card Product --}}
+                @include('components.frontend.card-product')
+
+            @empty
+                <h2>belum ada product</h2>
+            @endforelse
+        </section>
     </section>
-</section>
 
-{{-- Pagination --}}
-{{-- if the $products has more than one page (meaning there are multiple pages) --}}
-@if ($products->lastPage() > 1)
-{{ $products->links('components.frontend.pagination') }}
-@endif
+    {{-- Pagination --}}
+    {{-- if the $products has more than one page (meaning there are multiple pages) --}}
+    @if ($products->lastPage() > 1)
+        {{ $products->links('components.frontend.pagination') }}
+    @endif
 @endsection
 
 @push('script')
-<script>
-    document.addEventListener("DOMContentLoaded", function(evt) {
+    <script>
+        document.addEventListener("DOMContentLoaded", function(evt) {
 
             document.querySelectorAll(".title").forEach(function(el) {
                 el.addEventListener("click", function(evt) {
@@ -91,5 +90,5 @@ Produk Kami —
             });
 
         });
-</script>
+    </script>
 @endpush()
