52f922045ed35ec3fb7376871faf996ab710ad29
ramaherdianto <ramadhanherdianto35@gmail.com>
2024-04-27 19:56:13 +0700

    fix: fixed logo size with different images

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <img class="w-1/2 sm:w-[12rem] md:w-[10rem] lg:w-[14rem]"
+            <img class="max-w-[130px] max-h-[35px] sm:max-w-[150px] sm:max-h-[40px] md:max-w-[160px] md:max-h-[46px] lg:max-w-[350px] lg:max-h-[50px] object-contain"
                 src="{{ isset($info->logo) ? Storage::url($info->logo) : asset('assets/images/logo.png') }}"
                 alt="Percetakan Mandiri Print | Tempat di mana kreativitas bertemu dengan teknologi" />
         </x-slot>
