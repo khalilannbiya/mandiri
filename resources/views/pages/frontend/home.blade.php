e19a88e715227c98848b72fc04c4b64c5c334747
Syeich Khalil Annbiya <84386261+khalilannbiya@users.noreply.github.com>
2024-05-02 21:34:52 +0700

    fix: change logo default, card product component at home page (#47)
    Co-authored-by: syeichkhalil30@gmail.com <syeichkhalil@gmail.com>

--- resources/views/pages/frontend/home.blade.php
+++ resources/views/pages/frontend/home.blade.php
@@ -111,106 +111,13 @@
             <section class="flex flex-col justify-center items-center gap-6">
                 <section
                     class="w-full py-16 md:py-6 flex flex-wrap justify-center items-center gap-y-20 gap-x-8 xl:gap-x-12">
+
                     {{-- Card Prod --}}
-                    <article
-                        class="relative group flex w-[250px] xl:w-[280px] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md hover:shadow-2xl transition-all duration-700 ease-in-out">
-                        <div
-                            class="relative flex justify-center items-center mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-slate-400 to-slate-500">
-                            <img class="w-3/5 object-cover object-center aspect-video rounded-md"
-                                src="{{ asset('assets/images/default-image.png') }}">
-                        </div>
-                        <div class="p-6">
-                            <h5
-                                class="mb-2 block text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-                                Buku Amplop
-                            </h5>
-                            <p class="block text-base font-light leading-relaxed text-inherit antialiased">
-                                Rp. 69000
-                            </p>
-                        </div>
-                        <div class="p-6 pt-0 flex justify-between items-center">
-                            <a href="" class="underline">Detail</a>
-                            <a href="https://api.whatsapp.com/send?phone=6281290959055&text=Hello%20there!" target="_blank"
-                                data-ripple-light="true"
-                                class="flex items-center justify-between px-4 gap-3 select-none rounded-lg bg-slate-400 py-3 text-center align-middle text-sm font-bold text-white shadow-md shadow-slate-500/20 transition-all hover:shadow-lg hover:shadow-slate-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    style="fill: white;transform: ;msFilter:;">
-                                    <path
-                                        d="M21.822 7.431A1 1 0 0 0 21 7H7.333L6.179 4.23A1.994 1.994 0 0 0 4.333 3H2v2h2.333l4.744 11.385A1 1 0 0 0 10 17h8c.417 0 .79-.259.937-.648l3-8a1 1 0 0 0-.115-.921zM17.307 15h-6.64l-2.5-6h11.39l-2.25 6z">
-                                    </path>
-                                    <circle cx="10.5" cy="19.5" r="1.5"></circle>
-                                    <circle cx="17.5" cy="19.5" r="1.5"></circle>
-                                </svg>
-                                Beli
-                            </a>
-                        </div>
-                    </article>
-                    <article
-                        class="relative group flex w-[250px] xl:w-[280px] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md hover:shadow-2xl transition-all duration-700 ease-in-out">
-                        <div
-                            class="relative flex justify-center items-center mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-slate-400 to-slate-500">
-                            <img class="w-3/5 object-cover object-center aspect-video rounded-md"
-                                src="{{ asset('assets/images/default-image.png') }}">
-                        </div>
-                        <div class="p-6">
-                            <h5
-                                class="mb-2 block text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-                                Buku Amplop
-                            </h5>
-                            <p class="block text-base font-light leading-relaxed text-inherit antialiased">
-                                Rp. 69000
-                            </p>
-                        </div>
-                        <div class="p-6 pt-0 flex justify-between items-center">
-                            <a href="" class="underline">Detail</a>
-                            <a href="https://api.whatsapp.com/send?phone=6281290959055&text=Hello%20there!" target="_blank"
-                                data-ripple-light="true"
-                                class="flex items-center justify-between px-4 gap-3 select-none rounded-lg bg-slate-400 py-3 text-center align-middle text-sm font-bold text-white shadow-md shadow-slate-500/20 transition-all hover:shadow-lg hover:shadow-slate-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    viewBox="0 0 24 24" style="fill: white;transform: ;msFilter:;">
-                                    <path
-                                        d="M21.822 7.431A1 1 0 0 0 21 7H7.333L6.179 4.23A1.994 1.994 0 0 0 4.333 3H2v2h2.333l4.744 11.385A1 1 0 0 0 10 17h8c.417 0 .79-.259.937-.648l3-8a1 1 0 0 0-.115-.921zM17.307 15h-6.64l-2.5-6h11.39l-2.25 6z">
-                                    </path>
-                                    <circle cx="10.5" cy="19.5" r="1.5"></circle>
-                                    <circle cx="17.5" cy="19.5" r="1.5"></circle>
-                                </svg>
-                                Beli
-                            </a>
-                        </div>
-                    </article>
-                    <article
-                        class="relative group flex w-[250px] xl:w-[280px] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md hover:shadow-2xl transition-all duration-700 ease-in-out">
-                        <div
-                            class="relative flex justify-center items-center mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-slate-400 to-slate-500">
-                            <img class="w-3/5 object-cover object-center aspect-video rounded-md"
-                                src="{{ asset('assets/images/default-image.png') }}">
-                        </div>
-                        <div class="p-6">
-                            <h5
-                                class="mb-2 block text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-                                Buku Amplop
-                            </h5>
-                            <p class="block text-base font-light leading-relaxed text-inherit antialiased">
-                                Rp. 69000
-                            </p>
-                        </div>
-                        <div class="p-6 pt-0 flex justify-between items-center">
-                            <a href="" class="underline">Detail</a>
-                            <a href="https://api.whatsapp.com/send?phone=6281290959055&text=Hello%20there!"
-                                target="_blank" data-ripple-light="true"
-                                class="flex items-center justify-between px-4 gap-3 select-none rounded-lg bg-slate-400 py-3 text-center align-middle text-sm font-bold text-white shadow-md shadow-slate-500/20 transition-all hover:shadow-lg hover:shadow-slate-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    viewBox="0 0 24 24" style="fill: white;transform: ;msFilter:;">
-                                    <path
-                                        d="M21.822 7.431A1 1 0 0 0 21 7H7.333L6.179 4.23A1.994 1.994 0 0 0 4.333 3H2v2h2.333l4.744 11.385A1 1 0 0 0 10 17h8c.417 0 .79-.259.937-.648l3-8a1 1 0 0 0-.115-.921zM17.307 15h-6.64l-2.5-6h11.39l-2.25 6z">
-                                    </path>
-                                    <circle cx="10.5" cy="19.5" r="1.5"></circle>
-                                    <circle cx="17.5" cy="19.5" r="1.5"></circle>
-                                </svg>
-                                Beli
-                            </a>
-                        </div>
-                    </article>
+                    @forelse ($products as $product)
+                        @include('components.frontend.card-product')
+                    @empty
+                        @include('components.frontend.product-not-found')
+                    @endforelse
                 </section>
                 <section class="flex justify-center items-center">
                     <a href="{{ route('products') }}"
