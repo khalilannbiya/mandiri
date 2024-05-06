328766cd3fef86c973965f3a274d26af09f41c86
Syeich Khalil Annbiya <84386261+khalilannbiya@users.noreply.github.com>
2024-05-06 08:10:54 +0700

    fix:favicon editable guest layouts (#54)
    Co-authored-by: syeichkhalil30@gmail.com <syeichkhalil@gmail.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::composer(['components.frontend.navbar', 'components.frontend.footer', 'navigation-menu', 'auth.login', 'auth.register', 'layouts.app', 'layouts.frontend'], function ($view) {
+        View::composer(['components.frontend.navbar', 'components.frontend.footer', 'navigation-menu', 'auth.login', 'auth.register', 'layouts.app', 'layouts.frontend', 'layouts.guest'], function ($view) {
             $info = WebsiteInfo::first();
             $view->with('info', $info);
         });
